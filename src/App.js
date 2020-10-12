863c7858b9338d58e37408c973509dbaacca8b18
fccJashCoda <personalrevolution2014@gmail.com>
2020-10-12 22:34:15 +0200

    refactored App.js to use Hooks

--- src/App.js
+++ src/App.js
@@ -1,52 +1,38 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import './App.css';
 
 import InputBox from './components/InputBox';
 import TodoList from './components/TodoList';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
+function App() {
+  const [todos, setTodos] = useState([
+    { id: uuidv4(), item: 'Chocolate' },
+    { id: uuidv4(), item: 'Strawberry' },
+    { id: uuidv4(), item: 'Whipped Cream' },
+  ]);
 
-    this.state = {
-      todos: [
-        { id: uuidv4(), item: 'Chocolate' },
-        { id: uuidv4(), item: 'Strawberry' },
-        { id: uuidv4(), item: 'Whipped Cream' },
-      ],
-    };
+  const addTodo = (todo) => {
+    console.log(todo);
+    setTodos([...todos, { id: uuidv4(), item: todo }]);
+  };
 
-    this.addTodo = this.addTodo.bind(this);
-  }
+  const removeTodo = () => {
+    // this.setState(
+    //       {
+    //         todos: [...this.state.todos, { id: uuidv4(), item: todo }],
+    //       },
+    //       () => console.log(this.state.todos)
+    //     );
+  };
 
-  addTodo(todo) {
-    this.setState(
-      {
-        todos: [...this.state.todos, { id: uuidv4(), item: todo }],
-      },
-      () => console.log(this.state.todos)
-    );
-  }
-
-  removeTodo(todo) {
-    this.setState(
-      {
-        todos: this.state.todos.filter((item) => item.id !== todo.id),
-      },
-      () => console.log(this.state.todos)
-    );
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <h1>Questify</h1>
-        <InputBox addTodo={this.addTodo} />
-        <TodoList todos={this.state.todos} />
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <h1>Questify</h1>
+      <InputBox addTodo={addTodo} />
+      <TodoList todos={todos} />
+    </div>
+  );
 }
 
 export default App;
